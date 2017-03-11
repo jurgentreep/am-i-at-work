2d032947941c74fa1492c4c8696d5371901fb575
Jurgen Treep <jurgen@7lab.nl>
2017-03-11 13:54:53 +0100

    fix: moment duration doesn't get set between working hours feat: update time every second docs: add comments

--- js/app.js
+++ js/app.js
@@ -1,41 +1,58 @@
 var paragraph = document.querySelector('.javascript-date');
 
-var currentTime = moment.tz('Europe/Amsterdam');
-var startTime = moment.tz('Europe/Amsterdam').hours(9).minutes(0).seconds(0);
-var endTime = moment.tz('Europe/Amsterdam').hours(17).minutes(30).seconds(0);
-var dayNumber = currentTime.day();
-var isWeekend = dayNumber >= 6;
+function displayTime() {
+	// Variables to do my comparisons
+	var currentTime = moment.tz('Europe/Amsterdam');
+	var startTime = moment.tz('Europe/Amsterdam').hours(9).minutes(0).seconds(0);
+	var endTime = moment.tz('Europe/Amsterdam').hours(17).minutes(30).seconds(0);
+	var dayNumber = currentTime.day();
+	var isWeekend = dayNumber >= 6;
 
-switch(dayNumber) {
-	case 5:
-		var daysUntilNextWorkday = 3;
-		break;
-	case 6:
-		var daysUntilNextWorkday = 2;
-		break;
+	// Determining the amount of days until the nextworkday
+	// by the day number of the week
+	switch(dayNumber) {
+		case 5:
+			var daysUntilNextWorkday = 3;
+			break;
+		case 6:
+			var daysUntilNextWorkday = 2;
+			break;
 
-	default:
-		var daysUntilNextWorkday = 1;
-		break;
+		default:
+			var daysUntilNextWorkday = 1;
+			break;
+	}
+
+	// Creating the next workday datetime
+	var nextWorkDay = startTime.clone().add(daysUntilNextWorkday, 'days');
+
+	// Check if we are at work
+	if (currentTime >= startTime &&
+		currentTime <= endTime &&
+		isWeekend === false) {
+		var diff = currentTime.diff(endTime);
+
+		var sentence = 'Yes, I will be working for another ';
+	} else {
+		var sentence = 'No, I am going to work again in ';
+	}
+
+	// Check if we are before working hours
+	if (currentTime < startTime) {
+		var diff = moment.duration(startTime.diff(currentTime));
+	}
+
+	// Check if we are after working hours
+	if (currentTime > endTime || isWeekend === true) {
+		var diff = moment.duration(nextWorkDay.diff(currentTime));
+	}
+
+	// Printing the sentence and the time we've calculated
+	paragraph.textContent = sentence + diff.days() + ' days, ' + diff.hours() + ' hours, ' + diff.minutes() + ' minutes and ' + diff.seconds() + ' seconds.';
 }
 
-var nextWorkDay = startTime.clone().add(daysUntilNextWorkday, 'days');
+displayTime();
 
-if (currentTime >= startTime &&
-	currentTime <= endTime &&
-	isWeekend === false) {
-
-	var sentence = 'Yes, I will be working for another ';
-} else {
-	var sentence = 'No, I am going to work again in ';
-}
-
-if (currentTime < startTime) {
-	var diff = moment.duration(startTime.diff(currentTime));
-}
-
-if (currentTime > endTime || isWeekend === true) {
-	var diff = moment.duration(nextWorkDay.diff(currentTime));
-}
-
-paragraph.textContent = sentence + diff.days() + ' days, ' + diff.hours() + ' hours, ' + diff.minutes() + ' minutes and ' + diff.seconds() + ' seconds.';
+moment.duration(1, "seconds").timer({
+	loop: true
+}, displayTime);
